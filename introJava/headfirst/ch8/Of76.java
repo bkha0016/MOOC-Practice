ea8db40edf94139e711d54145adacbe51f09894c
bkha0016 <bryankhang123@gmail.com>
2023-10-13 16:11:49 +0700

    Finished chapter 8

---
+++ introJava/headfirst/ch8/Of76.java
@@ -0,0 +1,39 @@
+interface Nose {
+    
+    public int iMethod();
+
+}
+
+abstract class Picasso implements Nose {
+    public int iMethod() {
+        return 7;
+    }
+}
+
+class Acts implements Nose {
+    public int iMethod() {
+        return 5;
+    }
+
+}
+
+class Clowns extends Picasso {
+
+}
+
+
+public class Of76 extends Clowns{
+    public static void main(String[] args){
+        Nose[] i = new Nose[3];
+
+        i[0] = new Acts();
+        i[1] = new Clowns();
+        i[2] = new Of76();
+
+        for (int x = 0; x < 3; x++) {
+            System.out.println(i[x].iMethod() + " " + i[x].getClass());
+        }
+
+    }
+    
+}
