0da2e7122bd0efef6238de5fd723bb1d641ed051
bkha0016 <bryankhang123@gmail.com>
2024-02-02 16:15:38 +0700

    adding chapters

---
+++ introJava/headfirstOOP/ch04/dogDoors-Maria/DogDoor.java
@@ -0,0 +1,44 @@
+import java.util.Timer;
+import java.util.TimerTask;
+import java.util.LinkedList;
+import java.util.List;
+
+public class DogDoor {
+    private boolean open;
+    private List allowedBarks;
+
+    public DogDoor() {
+        this.allowedBarks = new LinkedList();
+        this.open = false;
+    }
+
+    public void addAllowedBark(Bark bark) {
+        allowedBarks.add(bark);
+    }
+
+    public List getAllowedBarks() {
+        return allowedBarks;
+    }
+
+    public void open() {
+        System.out.println("The dog door opens.");
+        open = true;
+
+        final Timer timer = new Timer();
+        timer.schedule(new TimerTask() {
+            public void run() {
+                close();
+                timer.cancel();
+            }
+        }, 5000);
+    }
+
+    public void close() {
+        System.out.println("The dog door closes.");
+        open = false;
+    }
+
+    public boolean isOpen() {
+        return open;
+    }
+}
