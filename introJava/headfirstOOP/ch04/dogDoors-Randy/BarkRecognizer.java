a7d6e003a5bd9002768a03048463e104d508156a
bkha0016 <bryankhang123@gmail.com>
2024-02-07 16:34:08 +0700

    working on ch05

---
+++ introJava/headfirstOOP/ch04/dogDoors-Randy/BarkRecognizer.java
@@ -0,0 +1,17 @@
+public class BarkRecognizer {
+    private DogDoor door;
+
+    public BarkRecognizer(DogDoor door) {
+        this.door = door;
+    }
+
+    public void recognize(String bark) {
+        System.out.println("    BarkRecognizer: Head a '" + bark + "'");
+
+        if (door.getAllowedBark().equals(bark)) {
+            door.open();
+        } else {
+            System.out.println("This dog is not allowed");
+        }
+    }
+}
