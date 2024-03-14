a3dc11ce9a768c1fce72cf928cc2f898e8c24342
bkha0016 <bryankhang123@gmail.com>
2024-03-14 20:20:58 +0700

    adding more in binary tree

--- introJava/basicJava/BinaryTree.java
+++ introJava/basicJava/BinaryTree.java
@@ -1,3 +1,5 @@
+import java.util.Stack;
+
 public class BinaryTree {
 
     private TreeNode root; // The first Node in the Binary Tree
@@ -16,8 +18,8 @@ public class BinaryTree {
     public void createBinaryTree() {
         TreeNode first = new TreeNode(9);
         TreeNode second = new TreeNode(2);
-        TreeNode third = new TreeNode(4);
-        TreeNode fourth = new TreeNode(3);
+        TreeNode third = new TreeNode(3);
+        TreeNode fourth = new TreeNode(4);
         TreeNode fifth = new TreeNode(5);
 
         root = first; // root --> first
@@ -31,19 +33,125 @@ public class BinaryTree {
 
     }
 
-    public void preOrder(TreeNode root) {
+    public void inOrder(){
+        if (root == null) {
+            return;
+        }
+        Stack<TreeNode> stack = new Stack<>();
+        TreeNode temp = root;
+        while(!stack.isEmpty() || temp != null) {
+            if(temp != null) {
+                stack.push(temp);
+                temp = temp.left;
+            } else {
+                temp = stack.pop();
+                System.out.print(temp.data + " ");
+                temp = temp.right;
+            }
+        }
+
+    }
+
+
+    public void recursiveInOrder(TreeNode root) {
+        if(root == null) {
+            return;
+        }
+        recursiveInOrder(root.left);
+        System.out.print(root.data + " ");
+        recursiveInOrder(root.right);
+    }
+
+    public void preOrder() {
+        if (root == null) {
+            return;
+        }
+        Stack<TreeNode> stack = new Stack<>();
+        stack.push(root); // There's a root in the stack
+
+        while(!stack.isEmpty()) {
+            TreeNode temp = stack.pop();
+            System.out.print(temp.data + " ");
+            if(temp.right != null) {
+                stack.push(temp.right);
+            }
+            if(temp.left != null) {
+                stack.push(temp.left);
+            }
+
+        }
+    }
+
+
+
+
+
+    public void recursivePreOrder(TreeNode root) {
         if (root == null) { // base case
             return;
         }
         System.out.print(root.data + " ");
-        preOrder(root.left);
-        preOrder(root.right);
+        recursivePreOrder(root.left);
+        recursivePreOrder(root.right);
+    }
+
+    public void recursivePostOrder(TreeNode root) {
+        if (root == null) {
+            return;
+        }
+        recursivePostOrder(root.left);
+        recursivePostOrder(root.right);
+        System.out.print(root.data + " ");
+    }
+
+    public void postOrder() {
+        TreeNode current = root;
+        Stack<TreeNode> stack = new Stack<>();
+
+        while(current != null || !stack.isEmpty()){
+            if(current != null){
+                stack.push(current);
+                current = current.left;
+            } else {
+                TreeNode temp = stack.peek().right;
+                if(temp == null){
+                    temp = stack.pop();
+                    System.out.print(temp.data + " ");
+
+                    while(!stack.isEmpty() && temp == stack.peek().right) {
+                        temp = stack.pop();
+                        System.out.print(temp.data + " ");
+
+                    }
+
+                } else {
+                    current = temp;
+
+                }
+
+            }
+
+        }
+        System.out.println(" ");
+
+
     }
 
     public static void main(String[] args) {
         BinaryTree testTree = new BinaryTree();
         testTree.createBinaryTree();
-        testTree.preOrder(testTree.root);
+        testTree.inOrder(); // 4 2 9 3
+        System.out.println(" ");
+        testTree.recursiveInOrder(testTree.root); // 4 2 9 3
+        System.out.println(" ");
+        testTree.preOrder();
+        System.out.println(" ");
+        testTree.recursivePreOrder(testTree.root); // 9 2 4 3
+        System.out.println(" ");
+        testTree.recursivePostOrder(testTree.root); // 4 2 3 9
+        System.out.println(" ");
+        testTree.postOrder(); // 4 2 3 9
+
         
     }
 
